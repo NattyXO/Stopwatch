ad878d133ab364962800c48e3356534efb0316ed
NattyXO <nattyxo2018@gmail.com>
2024-01-01 23:06:01 +0300

    v1.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace Stopwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.lblLap = new System.Windows.Forms.Label();
+            this.lblMinute = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.pnlBar = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnLap = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnFastTime = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.picClose = new System.Windows.Forms.PictureBox();
+            this.picInfo = new System.Windows.Forms.PictureBox();
+            this.picGithub = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picMinimize = new System.Windows.Forms.PictureBox();
+            this.pnlBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGithub)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMinimize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(114, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stop Watch";
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHours.ForeColor = System.Drawing.Color.Red;
+            this.lblHours.Location = new System.Drawing.Point(87, 81);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(60, 42);
+            this.lblHours.TabIndex = 1;
+            this.lblHours.Text = "00";
+            // 
+            // lblLap
+            // 
+            this.lblLap.AutoSize = true;
+            this.lblLap.ForeColor = System.Drawing.Color.White;
+            this.lblLap.Location = new System.Drawing.Point(278, 133);
+            this.lblLap.Name = "lblLap";
+            this.lblLap.Size = new System.Drawing.Size(59, 13);
+            this.lblLap.TabIndex = 2;
+            this.lblLap.Text = "lap Record";
+            // 
+            // lblMinute
+            // 
+            this.lblMinute.AutoSize = true;
+            this.lblMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinute.ForeColor = System.Drawing.Color.Red;
+            this.lblMinute.Location = new System.Drawing.Point(150, 81);
+            this.lblMinute.Name = "lblMinute";
+            this.lblMinute.Size = new System.Drawing.Size(60, 42);
+            this.lblMinute.TabIndex = 3;
+            this.lblMinute.Text = "00";
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeconds.ForeColor = System.Drawing.Color.Red;
+            this.lblSeconds.Location = new System.Drawing.Point(213, 81);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(60, 42);
+            this.lblSeconds.TabIndex = 4;
+            this.lblSeconds.Text = "00";
+            // 
+            // pnlBar
+            // 
+            this.pnlBar.BackColor = System.Drawing.Color.Blue;
+            this.pnlBar.Controls.Add(this.label2);
+            this.pnlBar.Controls.Add(this.pictureBox1);
+            this.pnlBar.Controls.Add(this.picMinimize);
+            this.pnlBar.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.pnlBar.Location = new System.Drawing.Point(-7, -2);
+            this.pnlBar.Name = "pnlBar";
+            this.pnlBar.Size = new System.Drawing.Size(380, 23);
+            this.pnlBar.TabIndex = 5;
+            this.pnlBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlBar_MouseDown);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
+            this.btnStart.Location = new System.Drawing.Point(14, 195);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(64, 27);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.Black;
+            this.btnStop.Location = new System.Drawing.Point(94, 195);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(64, 27);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnLap
+            // 
+            this.btnLap.BackColor = System.Drawing.Color.Aqua;
+            this.btnLap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLap.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLap.ForeColor = System.Drawing.Color.Black;
+            this.btnLap.Location = new System.Drawing.Point(14, 228);
+            this.btnLap.Name = "btnLap";
+            this.btnLap.Size = new System.Drawing.Size(64, 27);
+            this.btnLap.TabIndex = 8;
+            this.btnLap.Text = "Lap";
+            this.btnLap.UseVisualStyleBackColor = false;
+            this.btnLap.Click += new System.EventHandler(this.btnLap_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Red;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.Black;
+            this.btnReset.Location = new System.Drawing.Point(94, 228);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(64, 27);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnFastTime
+            // 
+            this.btnFastTime.BackColor = System.Drawing.Color.Yellow;
+            this.btnFastTime.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFastTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFastTime.ForeColor = System.Drawing.Color.Black;
+            this.btnFastTime.Location = new System.Drawing.Point(164, 228);
+            this.btnFastTime.Name = "btnFastTime";
+            this.btnFastTime.Size = new System.Drawing.Size(83, 27);
+            this.btnFastTime.TabIndex = 10;
+            this.btnFastTime.Text = "FastTime";
+            this.btnFastTime.UseVisualStyleBackColor = false;
+            this.btnFastTime.Click += new System.EventHandler(this.btnFastTime_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(43, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Stop Watch";
+            // 
+            // picClose
+            // 
+            this.picClose.BackColor = System.Drawing.Color.Blue;
+            this.picClose.Image = global::Stopwatch.Properties.Resources.icons8_close_500;
+            this.picClose.Location = new System.Drawing.Point(314, 1);
+            this.picClose.Name = "picClose";
+            this.picClose.Size = new System.Drawing.Size(31, 20);
+            this.picClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picClose.TabIndex = 13;
+            this.picClose.TabStop = false;
+            this.picClose.Click += new System.EventHandler(this.picClose_Click);
+            // 
+            // picInfo
+            // 
+            this.picInfo.Image = global::Stopwatch.Properties.Resources.icons8_info_240;
+            this.picInfo.Location = new System.Drawing.Point(48, 264);
+            this.picInfo.Name = "picInfo";
+            this.picInfo.Size = new System.Drawing.Size(29, 32);
+            this.picInfo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picInfo.TabIndex = 12;
+            this.picInfo.TabStop = false;
+            this.picInfo.Click += new System.EventHandler(this.picInfo_Click);
+            // 
+            // picGithub
+            // 
+            this.picGithub.Image = global::Stopwatch.Properties.Resources.icons8_github_500_;
+            this.picGithub.Location = new System.Drawing.Point(14, 264);
+            this.picGithub.Name = "picGithub";
+            this.picGithub.Size = new System.Drawing.Size(29, 32);
+            this.picGithub.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picGithub.TabIndex = 11;
+            this.picGithub.TabStop = false;
+            this.picGithub.Click += new System.EventHandler(this.picGithub_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox1.Image = global::Stopwatch.Properties.Resources.Untitled_design__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 24);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // picMinimize
+            // 
+            this.picMinimize.BackColor = System.Drawing.Color.Blue;
+            this.picMinimize.Image = global::Stopwatch.Properties.Resources.icons8_minus_96;
+            this.picMinimize.Location = new System.Drawing.Point(280, 2);
+            this.picMinimize.Name = "picMinimize";
+            this.picMinimize.Size = new System.Drawing.Size(31, 20);
+            this.picMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picMinimize.TabIndex = 14;
+            this.picMinimize.TabStop = false;
+            this.picMinimize.Click += new System.EventHandler(this.picMinimize_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(349, 306);
+            this.Controls.Add(this.picClose);
+            this.Controls.Add(this.picInfo);
+            this.Controls.Add(this.picGithub);
+            this.Controls.Add(this.btnFastTime);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnLap);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pnlBar);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.lblMinute);
+            this.Controls.Add(this.lblLap);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlBar.ResumeLayout(false);
+            this.pnlBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGithub)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMinimize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.Label lblLap;
+        private System.Windows.Forms.Label lblMinute;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Panel pnlBar;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnLap;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnFastTime;
+        private System.Windows.Forms.PictureBox picGithub;
+        private System.Windows.Forms.PictureBox picInfo;
+        private System.Windows.Forms.PictureBox picMinimize;
+        private System.Windows.Forms.PictureBox picClose;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stopwatch
+{
+    public partial class Form1 : Form
+    {
+        int H = 00;
+        int M = 00;
+        int S = 00;
+        private List<string> lapTimes = new List<string>(); // Create a list to store lap times
+
+        int originalInterval;
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblHours.Text = "00";
+
+            lblMinute.Text = "00";
+
+            lblSeconds.Text = "00";
+            // Store the original interval
+            originalInterval = timer1.Interval;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void btnFastTime_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) // Check if the timer is running
+            {
+                int minimumInterval = 1; // Define a minimum interval value
+
+                if (timer1.Interval > minimumInterval) // Check if current interval is greater than minimum
+                {
+                    timer1.Interval /= 2;
+                    if (timer1.Interval < minimumInterval)
+                    {
+                        timer1.Interval = minimumInterval;
+                    }
+                }
+                else
+                {
+                    timer1.Interval = minimumInterval; // Set to minimum if it's already less than minimum
+                }
+            }else
+            {
+                MessageBox.Show("Before you click Fast time, please start the time.");
+            }
+        }
+
+            private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            S++;
+            if (S >= 60)
+            {
+                M++;
+                S = 0;
+            }
+            if (M >= 60)
+            {
+                H++;
+                M = 0;
+            }
+
+            lblHours.Text = H.ToString().PadLeft(2, '0'); // Ensures two-digit display with leading zeros
+            lblMinute.Text = M.ToString().PadLeft(2, '0');
+            lblSeconds.Text = S.ToString().PadLeft(2, '0');
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblHours.Text = "00";
+
+            lblMinute.Text = "00";
+
+            lblSeconds.Text = "00";
+
+            // Stop the timer
+            timer1.Stop();
+
+            H = 0;
+            M = 0;
+            S = 0;
+            // Reset the interval to its original value
+            timer1.Interval = originalInterval;
+        }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void picMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void picGithub_Click(object sender, EventArgs e)
+        {
+            string githublink = "https://github.com/NattyXO";
+            Process.Start(githublink);
+
+        }
+
+        private void btnLap_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) // Check if the timer is running
+            {
+                string currentTime = $"{H.ToString().PadLeft(2, '0')}:{M.ToString().PadLeft(2, '0')}:{S.ToString().PadLeft(2, '0')}";
+
+                lapTimes.Add(currentTime); // Add current time to the lap times list
+
+                if (lapTimes.Count > 12)
+                {
+                    lapTimes.RemoveAt(0); // Remove the oldest record when exceeding 12
+                }
+
+                // Update the lblLap to display up to 12 lap times
+                lblLap.Text = string.Join(Environment.NewLine, lapTimes);
+            }
+            else
+            {
+                MessageBox.Show("Before recording time, start the time.");
+            }
+        }
+
+        private void pnlBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void picInfo_Click(object sender, EventArgs e)
+        {
+            string softwareName = "Stopwatch";
+            string softwareVersion = "1.0"; // Set the version of your software here
+
+            string message = $"Name: {softwareName}\nVersion: {softwareVersion}";
+            MessageBox.Show(message+"\nAhadu Tech");
+        }
+    }
+}
